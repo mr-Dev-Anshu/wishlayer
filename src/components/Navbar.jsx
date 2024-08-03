90dbd4ddca7ab9c0b11eed431ba12741e7706258
mr-Dev-Anshu <anshuies31@gmail.com>
2024-08-03 16:46:55 +0530

    changes 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import TemporaryDrawer from "./Sidebar";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="h-[80px] flex justify-between items-center px-4 border-b  border-gray-300">
       <div  >
         <div className="">
+          <Link href={"/"}>
           <Image className="w-12" src={logo} alt="logo" />
+          </Link>
           <p className="text-[8px] font-black w-fit  flex justify-center ">
             <span className="  ">WISH </span>
             <span className="text-red-600  ">LAYER</span>
