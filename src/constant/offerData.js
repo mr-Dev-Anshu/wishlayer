21001082bc3792f144965804642e0ce30988f92a
mr-Dev-Anshu <anshuies31@gmail.com>
2024-08-05 12:21:35 +0530

    fas

--- src/constant/offerData.js
+++ src/constant/offerData.js
@@ -3,7 +3,7 @@ import offer2 from "@/assets/offer2.png";
 import offer3 from "@/assets/offer3.png";
 import offer4 from "@/assets/offer4.png";
 export const offerData = [
-  { img: offer2, heading: "Decoration Service", to: "cake" },
+  { img: offer2, heading: "Decoration Service", to: "decoration" },
   { img: offer4, heading: "Room Stays", to: "room" },
   { img: offer3, heading: "Party Venue", to: "venue" },
   { img: offer1, heading: "Customized Cake", to: "cake" },
