2a22fdf191e963ce001efab9255a2b519ca80750
mr-Dev-Anshu <anshuies31@gmail.com>
2024-08-27 22:32:59 +0530

    asdf

--- src/constant/Location.js
+++ src/constant/Location.js
@@ -4,28 +4,44 @@ export const LocationData = [
     heading: "Gomati Nagar",
   },
   {
-    img: "https://firebasestorage.googleapis.com/v0/b/news-f534b.appspot.com/o/uploads%2Fd9d7b0b7fda6ab0388afb75e114a9fcf.png?alt=media&token=7dc6b43b-e5e1-4d25-9921-d179a648c44e",
-    heading: "Alambagh",
+    img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2017.41.55_eb67fb96.jpg?alt=media&token=1014e808-9f58-4904-8286-2e8ac6c00913",
+    heading: "Hazaratganj",
+  },
+  {
+    img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2017.41.55_eb67fb96.jpg?alt=media&token=1014e808-9f58-4904-8286-2e8ac6c00913",
+    heading: "Hazaratganj",
   },
 
   {
-    img: "https://firebasestorage.googleapis.com/v0/b/news-f534b.appspot.com/o/uploads%2F0eb3d86a5ce16c8c28004957a566aecd.png?alt=media&token=7a874f04-8a01-4f64-bdcd-d48a4128006b",
-    heading: "Mahangar",
+    img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2017.44.43_6044eec1.jpg?alt=media&token=c7a6a65d-0a48-4cff-b49a-e64c1328fb4e",
+    heading: "Gomatinagar",
   },
-  {
-    img: "https://firebasestorage.googleapis.com/v0/b/news-f534b.appspot.com/o/uploads%2F0eb3d86a5ce16c8c28004957a566aecd.png?alt=media&token=7a874f04-8a01-4f64-bdcd-d48a4128006b",
-    heading: "Mahangar",
-  },
-  {
-    img: "https://firebasestorage.googleapis.com/v0/b/news-f534b.appspot.com/o/uploads%2F0eb3d86a5ce16c8c28004957a566aecd.png?alt=media&token=7a874f04-8a01-4f64-bdcd-d48a4128006b",
-    heading: "Mahangar",
-  },
-  {
-    img: "https://firebasestorage.googleapis.com/v0/b/news-f534b.appspot.com/o/uploads%2F0eb3d86a5ce16c8c28004957a566aecd.png?alt=media&token=7a874f04-8a01-4f64-bdcd-d48a4128006b",
-    heading: "Mahangar",
-  },
-  {
-    img: "https://firebasestorage.googleapis.com/v0/b/news-f534b.appspot.com/o/uploads%2F0eb3d86a5ce16c8c28004957a566aecd.png?alt=media&token=7a874f04-8a01-4f64-bdcd-d48a4128006b",
-    heading: "Mahangar",
-  },
+  // {
+  //   img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2017.46.09_915f3d58.jpg?alt=media&token=97d34d7b-243a-4fbd-850e-22a22670b808",
+  //   heading: "Chowk",
+  // },
+  // {
+  //   img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2017.54.19_9c039fbe.jpg?alt=media&token=fb19ecb7-5b25-4f4a-a2ab-131b63902073",
+  //   heading: "Aliganj",
+  // },
+  // {
+  //   img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2018.01.04_e4b8d9a5.jpg?alt=media&token=cf5c1117-6fee-4e8b-ab9a-c83225e3b990",
+  //   heading: "Mahangar",
+  // },
+  // {
+  //   img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2018.05.10_ee6fa366.jpg?alt=media&token=72177dcf-e55b-4da6-a020-cf4f07010b3a",
+  //   heading: "Amar Shaheed path",
+  // },
+  // {
+  //   img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2018.08.05_535d0c1a.jpg?alt=media&token=43529bbc-4de0-4d9f-a6b4-7cab498129e8",
+  //   heading: "Janakipuram",
+  // },
+  // {
+  //   img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2018.10.20_c5c616db.jpg?alt=media&token=9deeb433-1d9f-46a8-91ad-b8771dd71a1b",
+  //   heading: "Indira Nagar",
+  // },
+  // {
+  //   img: "https://firebasestorage.googleapis.com/v0/b/wishlayer-e67a5.appspot.com/o/location%2FWhatsApp%20Image%202024-08-24%20at%2018.13.35_b801f4ff.jpg?alt=media&token=355f0d33-a5d6-427d-bcea-ca3c56d58c4c",
+  //   heading: "Sushant golf city",
+  // },
 ];
